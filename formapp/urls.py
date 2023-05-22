57fbd849af469c48c91f9d4a2cfda27f6d8e3454
Kool-Cool <omgrabpoint@gmail.com>
2023-05-22 07:41:05 +0530

    Trying for factorial
    views,py
    
    need how to make submit button run function using given input

--- formapp/urls.py
+++ formapp/urls.py
@@ -8,6 +8,8 @@ from . import views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("",views.home_page_view,name="Home"),
+    
+    path("factorial/<int:number>",views.factorial_view)
     # path("",views.HomePageView.as_view() , name= "Home")
     
 ]
--- formapp/views.py
+++ formapp/views.py
@@ -7,6 +7,8 @@ from .forms import FactForm #new
 
 # Create your views here.
 
+
+
 def home_page_view(request):
     
     form = FactForm()
@@ -14,6 +16,20 @@ def home_page_view(request):
     # print(request)
     context = {'form':form}
     return render(request , "home.html" , context)
+
+
+""" http://127.0.0.1:8000/factorial/15 """
+
+def factorial(number):
+    if number==0:
+        return 1 
+    else :
+        return number*factorial(number-1)
+    
+    
+def factorial_view(request, number):
+    result = factorial(int(number))
+    return HttpResponse(f"The factorial of {number} is {result}")
 
 # class HomePageView(TemplateView):
 #     template_name = "home.html"
